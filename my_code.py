5502a4e2ef798f0d080c2a3b870eca16019070e0
mishikag <23mgupta@athenian.org>
2020-09-24 16:31:03 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -6,9 +6,8 @@ def avg(user_list):
     return average
 
 if __name__ == '__main__':
-
     # test your fucntion with this print statement before writing the input loop
-    #print(avg([2, 4, 6]))    # Put the values you want to test in for x,y and z
+    #print(avg([2, 4, 6]))    # Put the values you want to test in for x,y and z\
 
     # Now, comment out the print statement and work on the code below
     # Remember to indent in this section
@@ -16,10 +15,25 @@ if __name__ == '__main__':
     # Write a loop to allow the user to input the values to be averaged
     num=int(input("how many numbers do you want the average of "))
     smallest = 1
-
-    while num != smallest:
+    while num != num:
         user_input = int(input("enter a number "))
         user_list.append(user_input)
-        smallest+=1
+        smallest +=1
     print(avg(user_list))
+
+#DIFF WAY
+#def avg(user_list):
+    #average = sum(user_list)/len(user_list)
+    #return average
+
+#user_list = []
+#while num != "done":
+    #num = input("enter a number to be averaged. if done type done ")
+    #if num == "done":
+        #break
+        #num=int(num)
+    #user_list.append(int(num))
+#print(avg(user_list)) 
+
+
 
